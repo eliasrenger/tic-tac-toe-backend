12c2414c2d9820598e9e2318a358a5e7021e9273
Elias Renger <elias.renger@gmail.com>
2024-11-09 00:29:11 +0100

    feat: limit api calls using throttle

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,11 +1,22 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { ThrottlerModule, ThrottlerGuard } from '@nestjs/throttler';
+import { APP_GUARD } from '@nestjs/core';
 import { QlearningModule } from './qlearning/qlearning.module';
 
 @Module({
-  imports: [QlearningModule],
+  imports: [
+    QlearningModule, 
+    ThrottlerModule.forRoot([{
+      ttl: 60000,
+      limit: 10,
+    }])
+  ],
   controllers: [AppController],
-  providers: [AppService],
+  providers: [AppService, {
+    provide: APP_GUARD,
+    useClass: ThrottlerGuard,
+  }],
 })
 export class AppModule {}
